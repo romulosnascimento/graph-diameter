45430bb1fed0fee1207525d6b7132580296539ec
Romulo Nascimento <roms.silva@gmail.com>
2020-09-12 18:53:14 -0300

    Update and rename Main.java to Diametro.java

--- Diametro.java
+++ Diametro.java
@@ -3,35 +3,64 @@ import java.util.*;
 
 import static java.lang.Integer.parseInt;
 
-class Main {
+class Diametro {
+
+//  Computação de um diâmetro de um grafo utilizadno o algoritmo de Floyd–Warshall.
+
     public static void main(String[] args) {
         try {
-            Scanner scanner = new Scanner(new InputStreamReader(System.in));
-            System.out.println("Digite o caminho para o arquivo de entrada: ");
-            String input = scanner.nextLine();
 
-            File inputFile = new File(input);
-            Scanner myReader = null;
-            myReader = new Scanner(inputFile);
-            boolean isFirstLine = true;
+//          Definição de variáveis.
+
+//          Matriz de adjacências.
             int graph[][] = new int[0][0];
+
+//          Matriz de predecessores.
             int path[][] = new int[0][0];
 
+//          Valor do diâmetro.
             int diameter = 0;
+
+//          Vértices diametrais.
             int diameterU = 0;
             int diameterV = 0;
+
+//          Quantidade de vértices no caminho mínimo entre os vértices diametrais.
             int diameterSize = 0;
+
+//          Índice dos vértices no caminho mínimo entre os vértices diametrais.
             String diameterPath = "";
 
+//          Leitura do arquivo de entrada.
+//          É possível inserir um caminho para um arquivo específico
+//          ou simplesmente teclar enter para usar um arquivo input.txt
+//          localizado no mesmo diretório.
+
+            Scanner scanner = new Scanner(new InputStreamReader(System.in));
+            System.out.println("Digite o caminho para o arquivo de entrada ou tecle enter para usar input.txt: ");
+            String input = scanner.nextLine();
+            input = input.isEmpty() ? "input.txt" : input;
+
+            File inputFile = new File(input);
+            System.out.println("Arquivo de entrada: " + input);
+
+            Scanner myReader = new Scanner(inputFile);
+
+//          Inserção dos valores de entrada na matriz de adjacências e de predecesores.
+            boolean isFirstLine = true;
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
+
+//              Na primeira linha, as matrizes são inicializadas de acordo com o número de vértices do grafo (n).
                 if (isFirstLine) {
                     String[] line1 = data.split(" ");
                     int n = parseInt(line1[0]);
-                    int m = parseInt(line1[1]);
                     graph = new int[n][n];
                     path = new int[n][n];
                     isFirstLine = false;
+
+//              Na segunda linha, os pesos são adicionados.
+//              Os pesos são comparados com o valor inicial do diâmetro para casos em que eles já são mínimos.
                 } else {
                     if (!data.isEmpty()) {
                         String[] edge = data.split(" ");
@@ -49,6 +78,9 @@ class Main {
                 }
             }
 
+//          As posições que correspondem ao caminho de um vértice para ele mesmo são preenchidas com 0.
+//          As demais posição, quando vazias, recebem o valor de peso máximo para os nossos casos (300).
+//          Custo O(v²)
             for (int u = 0; u < graph.length; u++) {
                 for (int v = 0; v < graph.length; v++) {
                     if (u == v) graph[v][v] = 0;
@@ -58,6 +90,9 @@ class Main {
                 }
             }
 
+//          Loop principal do algoritmo de Floyd–Warshall, onde os caminhos mínimos são computados.
+//          O valor do diâmetro e os vértices diametrais são atulizados e a matriz de predecessores é preenchida simultaneamente.
+//          Custo O(v³)
             for (int k = 0; k < graph.length; k++) {
                 for (int i = 0; i < graph.length; i++) {
                     for (int j = 0; j < graph.length; j++) {
@@ -74,9 +109,8 @@ class Main {
                 }
             }
 
-            System.out.println("OUTPUT");
-            System.out.println(diameter);
-            System.out.println((diameterU+1) + " " + (diameterV+1));
+//          Reconstrução do caminho mínimo entre os vértices diametrais.
+//          Custo O(v)
 
             int du = diameterU;
             int dv = diameterV;
@@ -90,9 +124,13 @@ class Main {
                 diameterSize++;
             }
 
+            System.out.println("OUTPUT");
+            System.out.println(diameter);
+            System.out.println((diameterU+1) + " " + (diameterV+1));
             System.out.println(diameterSize);
             System.out.println(diameterPath);
 
+//          Escrita do arquivo de saída (output.txt).
             BufferedWriter bw = null;
             File file = new File("output.txt");
 
@@ -109,9 +147,9 @@ class Main {
             bw.write("" + diameterSize);
             bw.newLine();
             bw.write(diameterPath);
-            System.out.println("Resultado escrito em output.txt");
-
             if(bw!=null) bw.close();
+
+            System.out.println("Resultado escrito em output.txt");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
