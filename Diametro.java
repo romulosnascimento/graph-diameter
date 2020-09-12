94e04136892928eb5279cc296117824afb9b063c
Romulo Nascimento <roms.silva@gmail.com>
2020-09-12 20:34:57 -0300

    Update Diametro.java

--- Diametro.java
+++ Diametro.java
@@ -1,11 +1,13 @@
-import java.io.*;
 import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
 
 import static java.lang.Integer.parseInt;
 
 class Diametro {
 
-//  Computação de um diâmetro de um grafo utilizadno o algoritmo de Floyd–Warshall.
+//  Computação de um diâmetro de um grafo utilizando o algoritmo de Floyd–Warshall.
 
     public static void main(String[] args) {
         try {
@@ -32,50 +34,45 @@ class Diametro {
             String diameterPath = "";
 
 //          Leitura do arquivo de entrada.
-//          É possível inserir um caminho para um arquivo específico
-//          ou simplesmente teclar enter para usar um arquivo input.txt
-//          localizado no mesmo diretório.
+            
+            IO io = new IO();
+     	    String s = io.next();
+            int inputPosition = 1;
 
-            Scanner scanner = new Scanner(new InputStreamReader(System.in));
-            System.out.println("Digite o caminho para o arquivo de entrada ou tecle enter para usar input.txt: ");
-            String input = scanner.nextLine();
-            input = input.isEmpty() ? "input.txt" : input;
+            //     	IO io = new IO();
+//        while (s != null) {
+//            io.write(s + " ");
+//            s = io.next();
+//        }
+//
+//     	io.flush();
 
-            File inputFile = new File(input);
-            System.out.println("Arquivo de entrada: " + input);
+//          Inserção dos valores de entrada na matriz de adjacências e de predecessores.
 
-            Scanner myReader = new Scanner(inputFile);
-
-//          Inserção dos valores de entrada na matriz de adjacências e de predecesores.
-            boolean isFirstLine = true;
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-
+            while (s != null) {
 //              Na primeira linha, as matrizes são inicializadas de acordo com o número de vértices do grafo (n).
-                if (isFirstLine) {
-                    String[] line1 = data.split(" ");
-                    int n = parseInt(line1[0]);
+                if (inputPosition == 1) {
+                    int n = parseInt(s);
                     graph = new int[n][n];
                     path = new int[n][n];
-                    isFirstLine = false;
 
 //              Na segunda linha, os pesos são adicionados.
 //              Os pesos são comparados com o valor inicial do diâmetro para casos em que eles já são mínimos.
-                } else {
-                    if (!data.isEmpty()) {
-                        String[] edge = data.split(" ");
-                        int u = parseInt(edge[0]) - 1;
-                        int v = parseInt(edge[1]) - 1;
-                        int w = parseInt(edge[2]);
-                        graph[u][v] = w;
-                        graph[v][u] = w;
-                        if (graph[u][v] > diameter) {
-                            diameter = graph[u][v];
-                            diameterU = u;
-                            diameterV = v;
-                        }
+                } else if (inputPosition > 2) {
+                    int u = parseInt(s) - 1;
+                    int v = parseInt(io.next()) - 1;
+                    int w = parseInt(io.next());
+                    graph[u][v] = w;
+                    graph[v][u] = w;
+                    if (graph[u][v] > diameter) {
+                        diameter = graph[u][v];
+                        diameterU = u;
+                        diameterV = v;
                     }
                 }
+
+                inputPosition++;
+                s = io.next();
             }
 
 //          As posições que correspondem ao caminho de um vértice para ele mesmo são preenchidas com 0.
@@ -91,7 +88,7 @@ class Diametro {
             }
 
 //          Loop principal do algoritmo de Floyd–Warshall, onde os caminhos mínimos são computados.
-//          O valor do diâmetro e os vértices diametrais são atulizados e a matriz de predecessores é preenchida simultaneamente.
+//          O valor do diâmetro e os vértices diametrais são atualizados e a matriz de predecessores é preenchida simultaneamente.
 //          Custo O(v³)
             for (int k = 0; k < graph.length; k++) {
                 for (int i = 0; i < graph.length; i++) {
@@ -124,37 +121,65 @@ class Diametro {
                 diameterSize++;
             }
 
-            System.out.println("OUTPUT");
-            System.out.println(diameter);
-            System.out.println((diameterU+1) + " " + (diameterV+1));
-            System.out.println(diameterSize);
-            System.out.println(diameterPath);
+//          Escrita do arquivo de saída.
 
-//          Escrita do arquivo de saída (output.txt).
-            BufferedWriter bw = null;
-            File file = new File("output.txt");
+            io.write("" + diameter);
+            io.newLine();
+            io.write((diameterU+1) + " " + (diameterV+1));
+            io.newLine();
+            io.write("" + diameterSize);
+            io.newLine();
+            io.write(diameterPath);
+            io.flush();
 
-            if (!file.exists()) {
-                file.createNewFile();
-            }
-
-            FileWriter fw = new FileWriter(file);
-            bw = new BufferedWriter(fw);
-            bw.write("" + diameter);
-            bw.newLine();
-            bw.write((diameterU+1) + " " + (diameterV+1));
-            bw.newLine();
-            bw.write("" + diameterSize);
-            bw.newLine();
-            bw.write(diameterPath);
-            if(bw!=null) bw.close();
-
-            System.out.println("Resultado escrito em output.txt");
-
-        } catch (FileNotFoundException e) {
+        } catch (Error e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+    }
+
+    public static class IO {
+        String [] line;
+        int i;
+        InputStreamReader ir;
+        BufferedReader in;
+        OutputStreamWriter or;
+        BufferedWriter out;
+        IO(){
+            ir = new InputStreamReader(System.in);
+            in = new BufferedReader(ir);
+            or = new OutputStreamWriter(System.out);
+            out = new BufferedWriter(or);
+        }
+        String next(){
+            try{
+                if (line == null || i == line.length){
+                    line = in.readLine().split(" ");
+                    i = 0;
+                }
+                return line[i++];
+            }catch (Exception e){ return null; }
+        }
+        Integer nextInt(){
+            return Integer.valueOf(next());
+        }
+        Long nextLong(){
+            return Long.valueOf(next());
+        }
+        <T> void write(T t) {
+            try{
+                out.append(String.valueOf(t));
+            }catch (Exception e){}
+        }
+        public void flush(){
+            try{
+                out.flush();
+            }catch(IOException e){}
+        }
+
+        public void newLine() {
+            try{
+                out.append(String.valueOf("\n"));
+            }catch (Exception e){}
         }
     }
 }
